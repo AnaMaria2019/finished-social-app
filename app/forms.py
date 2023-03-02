33f9365c3b54dc57089ce47fbb2d9f631f026ba7
Ana <ana.maria.adina98@gmail.com>
2023-03-02 15:51:47 +0200

    Add empty line

--- app/forms.py
+++ app/forms.py
@@ -18,6 +18,7 @@ class UserProfileForm(forms.ModelForm):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     e_mail = forms.CharField(max_length=100)
+
     class Meta:
         model = models.UserProfile
         exclude = ['user', 'friend_requests', 'friends']
